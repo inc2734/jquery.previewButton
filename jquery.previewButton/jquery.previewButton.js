80b83c4f7fbb9a73c4fa2d5c4b0f5d20eaa37b33
inc2734 <inc@2inc.org>
2012-05-12 08:19:27 +0900

    Licence Changed.

--- jquery.previewButton/jquery.previewButton.js
+++ jquery.previewButton/jquery.previewButton.js
@@ -1,13 +1,28 @@
-/*
-* jquery.previewButton.js ver.1.1
-* Author : http://2inc.org
-* created: 2011/12/09
-* modified: 2011/12/13
-* creative commons 表示 - 継承 3.0 (CC BY-SA 3.0)
-* http://creativecommons.org/licenses/by-sa/3.0/deed.ja
-*
-* プレビュー画面を表示するjQueryプラグイン
-*/
+/**
+ * jquery.previewButton.js
+ * Description: プレビュー画面を表示するjQueryプラグイン
+ * Version: 1.2
+ * Author: Takashi Kitajima
+ * Autho URI: http://2inc.org
+ * created: Dec 9, 2011
+ * modified : May 12, 2012
+ * License: GPL2
+ *
+ * Copyright 2012 Takashi Kitajima (email : inc@2inc.org)
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 ( function( $ ) {
 	$.fn.previewButton = function( config ) {
 		var button = this;
